118fa6d6b972685da4f9d049ca8dd2d0a891859a
Phoenix.C <phoenix.fec@gmail.com>
2019-02-01 13:49:44 +0800

    Feat(Options/Background): add "Options Config" and "CORS ResponseHeaders Config" logic

--- options/options.js
+++ options/options.js
@@ -23,13 +23,36 @@ $(function() {
     // -------------------------------
     $('#xCustomConfigSave').click(function() {
         const config = {};
-        config.AccessControlAllowOrigin = $('#xCustomOrigin').val();
-        config.AccessControlAllowHeaders = $('#xCustomHeader').val();
-        config.AccessControlAllowMethod = $('#xCustomMethod').val();
+        config['Access-Control-Allow-Origin'] = $('#xCustomOrigin').val();
+        config['Access-Control-Allow-Headers'] = $('#xCustomHeaders').val();
+        config['Access-Control-Allow-Method'] = $('#xCustomMethod').val();
+        config['webRequestIncludeTypes'] = $('#xCustomIncludeTypes').val();
+        config['webRequestExcludeTypes'] = $('#xCustomExcludeTypes').val();
         // 上传云
         chrome.extension.getBackgroundPage().setStorage(config, 'sync', function() {
-            console.log('成功保存跨域配置，并且云同步')
+            // console.log('已成功保存跨域配置，并同步到云', config)
+            $('#savedMsg').text('已成功保存跨域配置，并同步到云');
+
+            // 将保存好的options数据发送给插件
+            chrome.runtime.sendMessage(config, function(response) {
+                console.log(response);
+            });
         });
     });
+
+    // 从选项同步的云上获取当前选项配置, 并缓存
+    chrome.extension.getBackgroundPage().getStorage([
+        'Access-Control-Allow-Origin',
+        'Access-Control-Allow-Method',
+        'Access-Control-Allow-Headers',
+        'webRequestIncludeTypes',
+        'webRequestExcludeTypes'
+    ], 'sync', result => {
+        $('#xCustomOrigin').val(result['Access-Control-Allow-Origin']);
+        $('#xCustomHeaders').val(result['Access-Control-Allow-Headers']);
+        $('#xCustomMethod').val(result['Access-Control-Allow-Method']);
+        $('#xCustomIncludeTypes').val(result['webRequestIncludeTypes']);
+        $('#xCustomExcludeTypes').val(result['webRequestExcludeTypes']);
+    }).catch(err => console.log(err));
 
 })
--- utils/background.js
+++ utils/background.js
@@ -1,25 +1,26 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-    window.CONTEXTMENU_ID = 'CORS_HTTP_SWITCH';
-    let isShowBadge = false;
-    // getStorage('showBadge', function(result) {
-    //     isShowBadge = result.showBadge;
-    // });
+    window.globalVar = {
+        CONTEXTMENU_ID: 'CORS_HTTP_SWITCH',
+        tmpResponseHeaders: {},
+        isShowBadge: false
+    };
+    // window.CONTEXTMENU_ID = 'CORS_HTTP_SWITCH';
+    // window.tmpResponseHeaders = {};
+    // let isShowBadge = false;
 
-    // ---------------------------------------
-    // 创建页面右键菜单
-    // ---------------------------------------
-    chrome.contextMenus.create({
-        title: '打开CORS跨域',
-        id: CONTEXTMENU_ID,
-        type: 'checkbox',
-        checked: isShowBadge
-    }, function() {
-        if (chrome.extension.lastError) {
-            console.log("Got expected error: " + chrome.extension.lastError.message);
-        }
-    });
+    // 从选项同步的云上获取当前选项配置, 并缓存
+    getStorage([
+        'Access-Control-Allow-Origin',
+        'Access-Control-Allow-Method',
+        'Access-Control-Allow-Headers',
+        'webRequestIncludeTypes',
+        'webRequestExcludeTypes'
+    ], 'sync', result => {
+        window.globalVar.tmpResponseHeaders = Object.assign({}, window.globalVar.tmpResponseHeaders, result);
+    }).catch(err => console.log(err));
+
 
     // 监听右键菜单
     chrome.contextMenus.onClicked.addListener(switchBadge);
@@ -27,7 +28,29 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.browserAction.onClicked.addListener(switchBadge);
     // 监听当前开启状态存储值的变更
     chrome.storage.onChanged.addListener(function(changes, namespace) {
-        isShowBadge = changes["showBadge"].newValue;
+        if(changes["showBadge"]) window.globalVar.isShowBadge = changes["showBadge"].newValue;
+    });
+    // 监听来自选项最新更新的配置数据
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        if(sender.id === 'fjmicobohjblipgaklggfkcdkomdgglk') {
+            window.globalVar.tmpResponseHeaders = Object.assign({}, window.globalVar.tmpResponseHeaders, request);
+            // console.log(window.globalVar.tmpResponseHeaders, request, sender)
+        }
+    });
+
+
+    // ---------------------------------------
+    // 创建页面右键菜单
+    // ---------------------------------------
+    chrome.contextMenus.create({
+        title: '打开CORS跨域',
+        id: window.globalVar.CONTEXTMENU_ID,
+        type: 'checkbox',
+        checked: window.globalVar.isShowBadge
+    }, function() {
+        if (chrome.extension.lastError) {
+            console.log("Got expected error: " + chrome.extension.lastError.message);
+        }
     });
 
     // ---------------------------------------
@@ -36,13 +59,12 @@ chrome.runtime.onInstalled.addListener(function() {
     // ---------------------------------------
     chrome.webRequest.onHeadersReceived.addListener(
         function(details) {
-            // 过滤掉非当前tab内网页请求
-            // ...
-            if(isShowBadge) {
-                return webRequestOnHeadersReceived(details);
+            if(window.globalVar.isShowBadge) {
+                return setResponseHeader(details, window.globalVar.tmpResponseHeaders);
             }
         },{urls: ["<all_urls>"]}, ["responseHeaders","blocking"]
     );
+
 });
 
 
@@ -53,39 +75,103 @@ function switchBadge() {
         if(!result.showBadge) {
             chrome.browserAction.setBadgeText({text: 'On'});
             chrome.browserAction.setBadgeBackgroundColor({color: [205, 70, 70, 255]});
-            chrome.contextMenus.update(CONTEXTMENU_ID, {title: '关闭CORS跨域', checked: true});
+            chrome.contextMenus.update(window.globalVar.CONTEXTMENU_ID, {title: '关闭CORS跨域', checked: true});
             chrome.storage.local.set({ 'showBadge': true });
         } else {
             chrome.browserAction.setBadgeText({text: ''});
             chrome.browserAction.setBadgeBackgroundColor({color: [0, 0, 0, 0]});
-            chrome.contextMenus.update(CONTEXTMENU_ID, {title: '打开CORS跨域', checked: false});
+            chrome.contextMenus.update(window.globalVar.CONTEXTMENU_ID, {title: '打开CORS跨域', checked: false});
             chrome.storage.local.set({ 'showBadge': false });
         }
     });
 }
 
-// 监听篡改响应头cors的callback
-function webRequestOnHeadersReceived(details) {
+/**
+ * 将自定义配置赋值给responseHeaders
+ * @param {object} details chrome.webRequest.onHeadersReceived.addListener callback
+ * @param {object} config 自定义配置数据
+ * @returns new details.responseHeaders
+ */
+function setResponseHeader(details, config) {
+
+    const includeTypes = config.webRequestIncludeTypes && config.webRequestIncludeTypes.replace(/ /g, '').split(',');
+    console.log(details, config, config.webRequestIncludeTypes, includeTypes)
+
+    if(hasCorsHeader(details.responseHeaders, 'Access-Control-Allow-Origin')) {
+        const curKeyIndex = hasCorsHeader(details.responseHeaders, 'Access-Control-Allow-Origin', true);
+        details.responseHeaders[curKeyIndex].value = config['Access-Control-Allow-Origin'];
+    }
+    else {
+        details.responseHeaders.push({
+            name: 'Access-Control-Allow-Origin',
+            value: config['Access-Control-Allow-Origin']
+        });
+    }
+
+    if(hasCorsHeader(details.responseHeaders, 'Access-Control-Allow-Method')) {
+        const curKeyIndex = hasCorsHeader(details.responseHeaders, 'Access-Control-Allow-Method', true);
+        details.responseHeaders[curKeyIndex].value = config['Access-Control-Allow-Method'];
+    }
+    else {
+        details.responseHeaders.push({
+            name: 'Access-Control-Allow-Method',
+            value: config['Access-Control-Allow-Method']
+        });
+    }
+
+    if(hasCorsHeader(details.responseHeaders, 'Access-Control-Allow-Headers')) {
+        const curKeyIndex = hasCorsHeader(details.responseHeaders, 'Access-Control-Allow-Headers', true);
+        details.responseHeaders[curKeyIndex].value = config['Access-Control-Allow-Headers'];
+    }
+    else {
+        details.responseHeaders.push({
+            name: 'Access-Control-Allow-Headers',
+            value: config['Access-Control-Allow-Headers']
+        });
+    }
+
     return { responseHeaders: details.responseHeaders };
 }
 
+/**
+ * 鉴别当前响应头是否有指定的头信息
+ * @param {array} arr responseHeaders
+ * @param {string} key responseHeaders[*].name
+ * @param {boolean} returnIndex 'false' is default.
+ * @returns {boolean}
+ */
+function hasCorsHeader(arr, key, returnIndex) {
+    let isHere = -1;
+    if(Object.prototype.toString.call([]) === '[object Array]') {
+        isHere = arr.findIndex((item) => {
+            item.name && item.name === key;
+        });
+    }
+    return  returnIndex ? isHere : isHere > -1;
+}
 
 /**
  * 获取指定key当前值，可指定作用域
- * @param {string} key
+ * @param {string | array} key
  * @param {?string: local | sync} scope 可选，默认存储到storage.local
  * @param {?function} callback
+ * @returns promise
  */
 function getStorage(key, scope, callback) {
     const curScope = arguments.length > 2 ? scope : 'local';
     const curCallback = arguments.length > 2 ? callback : scope;
-    chrome.storage[curScope].get(key, function(result) {
-        if (chrome.extension.lastError) {
-            console.log("Got expected error: " + chrome.extension.lastError.message);
-            return null;
-        }
-        if(typeof curCallback === 'function') curCallback(result);
+    const promise = new Promise((resolve, reject) => {
+        chrome.storage[curScope].get(key, function(result) {
+            if (chrome.extension.lastError) {
+                console.log("Got expected error: " + chrome.extension.lastError.message);
+                return null;
+            }
+            if(typeof curCallback === 'function') {
+                resolve(curCallback(result));
+            }
+        });
     });
+    return promise;
 }
 
 /**
